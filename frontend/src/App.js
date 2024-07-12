654547a29f6e9849e5ae50ed2125d8551a32b988
mishalmusthafa <mishalmusthafa12@gmail.com>
2024-07-12 14:43:10 +0400

    Added featured products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,37 @@
+import data from './data';
+
 function App() {
     return (
         <div>
             <header>
                 <a href="/">Amazona</a>
             </header>
-            <main>Listings</main>
+            <main>
+                <h1>Featured Items</h1>
+                <div className="products">
+                    {data.products.map((product) => (
+                        <div className="product" key={product.slug}>
+                            <a href={`/product/${product.slug}`}>
+                                <div>
+                                    <img
+                                        src={product.image}
+                                        alt={product.name}
+                                    />
+                                </div>
+                            </a>
+                            <div className="product-info">
+                                <a href={`/product/${product.slug}`}>
+                                    <p>{product.name}</p>
+                                </a>
+                                <p>
+                                    <strong>${product.price} </strong>
+                                </p>
+                                <button>Add to cart</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </main>
         </div>
     );
 }
